6ce5b3004cb914e501537de327a2086ee3b96d01
Kelly Nelson <kelnel86@gmail.com>
2022-09-19 15:41:28 -0500

    finishes creating components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar";
 import Reviews from "./components/Reviews";
 import AverageRating from "./components/AverageRating";
 import SentimentAnalysis from './components/SentimentAnalysis';
+import WebsiteVisitors from './components/WebsiteVisitors';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Reviews number="1,281"/>
       <AverageRating average="4.6"/>
       <SentimentAnalysis />
+      <WebsiteVisitors visitors="821"/>
     </div>
   );
 }
---
+++ src/components/WebsiteVisitors.js
@@ -0,0 +1,12 @@
+
+const WebsiteVisitors = (props) => {
+    return (
+        <div>
+            <p>Website Visitors</p>
+            <h3>{props.visitors}</h3>
+            <div className="rectangle">Hello</div>
+        </div>
+    )
+}
+
+export default WebsiteVisitors;
