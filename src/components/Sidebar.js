bd3f2afd5de25d983409b929630554f7ef6251f8
Kelly Nelson <kelnel86@gmail.com>
2022-09-19 15:00:48 -0500

    creates sidebar components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+const Sidebar = () => {
+  return (
+    <div class="sidebar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Widget</li>
+        <li>Reviews</li>
+        <li>Customers</li>
+        <li>Online Analysis</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
